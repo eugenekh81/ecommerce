fb137ec7fc7f29ec8a9f0007f050686474490153
eugenekh81 <eugene.khablenko@gmail.com>
2025-03-06 14:17:06 +0200

    added layout, router, drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useEffect, useState } from 'react';
+import { ProductIS } from './types/ProductIS';
+import { ProductCard } from './components/ProductCard';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App: React.FC = () => {
+  const [products, setProducts] = useState<ProductIS[] | []>([]);
+
+  const loadData = async () => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
     </>
-  )
-}
-
-export default App
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import './index.css';
+import { HomePage } from './pages/HomePage/';
+import { AboutPage } from './pages/AboutPage/';
+import { Layout } from './pages/Layout/Layout.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      }
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <RouterProvider router={router} />
+);

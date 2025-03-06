63fabfa678fa677ff77d1585de6b08f0fb8c2bc4
eugenekh81 <eugene.khablenko@gmail.com>
2025-03-06 15:57:46 +0200

    added styles to product card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { ProductIS } from '../../types/ProductIS';
+import { Rating } from '@mui/material'
 
 export const ProductCard: React.FC<ProductIS> = ({
   title,
@@ -8,13 +9,18 @@ export const ProductCard: React.FC<ProductIS> = ({
   rating,
 }) => {
   return (
-    <div className='flex flex-col items-center justify-center bg-[#fff] w-[300px] gap-[30px]' >
+    <div className='flex flex-col items-center  bg-[#fff] w-[300px] gap-[30px] p-8'>
       <img src={image} alt={title} className='h-[160px]' />
-      <h2 className='text-[#212121]'>{title}</h2>
+      <h2 className='text-[#212121] font-bold text-[18px]'>{title}</h2>
       <p className='text-[#212121]'>{description}</p>
-      <p className='text-[#212121]'>${price}</p>
-      <p className='text-[#212121]'>{rating.rate}</p>
-      <p className='text-[#212121]'>{rating.count}</p>
+      <div className='text-[#212121] flex justify-between w-full'>
+        <p>Price:</p>
+        <p>${price}</p>
+      </div>
+      <div className='text-[#212121] flex justify-between w-full'>
+        <Rating name='read-only' value={rating.rate} readOnly precision={0.1} />
+        <p>Reviews: {rating.count}</p>
+      </div>
     </div>
   );
 };

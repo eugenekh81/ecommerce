b03470f647ba1d3427bff40e94308b6050f54ed4
eugenekh81 <eugene.khablenko@gmail.com>
2025-03-06 14:45:06 +0200

    fixed routing for about page

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -23,7 +23,7 @@ export const Layout: React.FC = () => {
                 <NavLink to='/'>Home</NavLink>
               </li>
               <li>
-                <NavLink to='/about'>About</NavLink>
+                <NavLink to='about'>About</NavLink>
               </li>
             </ul>
           </nav>
